905da3756e3f0064fc57cd652d3f1b99253c5a83
Jordy Aaldering <aalderingjordy@gmail.com>
2025-09-25 14:47:44 +0200

    cleanup

--- test.sh
+++ test.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
-./bin/flash || exit 1
-./bin/flash_p || exit 1
-./bin/flash_mt -mt 4 || exit 1
+./bin/flash
+./bin/flash_p
+./bin/flash_mt -mt 4
 
-./bin/fmg 2048 || exit 1
-#./bin/fmg_p 2048 || exit 1
-./bin/fmg_mt -mt 4 2048 || exit 1
+./bin/fmg
+#./bin/fmg_p
+./bin/fmg_mt -mt 4
 
-./bin/mandelbrot || exit 1
-./bin/mandelbrot_p || exit 1
-./bin/mandelbrot_mt -mt 4 || exit 1
+./bin/mandelbrot
+./bin/mandelbrot_p
+./bin/mandelbrot_mt -mt 4
 
-./bin/matmul || exit 1
-./bin/matmul_p || exit 1
-./bin/matmul_mt -mt 4 || exit 1
+./bin/matmul
+./bin/matmul_p
+./bin/matmul_mt -mt 4
 
-./bin/nbody || exit 1
-#./bin/nbody_p || exit 1
-./bin/nbody_mt -mt 4 || exit 1
+./bin/nbody
+#./bin/nbody_p
+./bin/nbody_mt -mt 4
 
-./bin/quickhull || exit 1
-./bin/quickhull_p || exit 1
-./bin/quickhull_mt -mt 4 || exit 1
+./bin/quickhull
+./bin/quickhull_p
+./bin/quickhull_mt -mt 4
 
-./bin/stencil || exit 1
-./bin/stencil_p || exit 1
-./bin/stencil_mt -mt 4 || exit 1
+./bin/stencil
+./bin/stencil_p
+./bin/stencil_mt -mt 4
